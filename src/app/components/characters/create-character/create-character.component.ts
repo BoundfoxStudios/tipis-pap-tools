0c5597c6ddd6b08825d69368ac35b4f65009829f
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-01-26 13:58:11 +0100

    feat: show info when character creation is not possible

--- src/app/components/characters/create-character/create-character.component.ts
+++ src/app/components/characters/create-character/create-character.component.ts
@@ -3,13 +3,13 @@ import { H1Component } from '../../headings/h1/h1.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TextInputComponent } from '../../inputs/text-input/text-input.component';
 import { SubmitButtonComponent } from '../../inputs/submit-button/submit-button.component';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CharacterStore } from '../../../stores/character.store';
 
 @Component({
     selector: 'pap-create-character',
     standalone: true,
-    imports: [H1Component, TextInputComponent, SubmitButtonComponent, FormsModule, ReactiveFormsModule],
+    imports: [H1Component, TextInputComponent, SubmitButtonComponent, FormsModule, ReactiveFormsModule, RouterLink],
     templateUrl: './create-character.component.html',
     styleUrl: './create-character.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,8 +18,8 @@ export default class CreateCharacterComponent {
     protected readonly formGroup = inject(FormBuilder).group({
         name: ['', Validators.required],
     });
+    protected readonly characterStore = inject(CharacterStore);
     private readonly router = inject(Router);
-    private readonly characterStore = inject(CharacterStore);
 
     protected async submit(): Promise<void> {
         if (this.formGroup.invalid) {
